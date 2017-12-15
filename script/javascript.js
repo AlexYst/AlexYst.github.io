34fec2da0712ec6e8d2d37750091766483611613
Alex Yst <git@y.st>
2017-12-14 17:20:58 -0800

    2017-12-13

---
+++ script/javascript.js
@@ -0,0 +1,38 @@
+/*
+	<https://y.st./>
+	Copyright © 2015-2016 //y.st. <mailto:copyright@y.st>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org./licenses/>.
+*/
+
+// This function causes the left and right arrow keys to load previous
+// and next pages in a sequence. As of writing, this is only used on
+// the weblog pages.
+function document__onkeyup(event) {
+	switch(event.keyCode) {
+		case 37: // left arrow key
+			var anchor = document.querySelector("a[rel=prev]");
+			if(anchor) {
+				document.location.href = anchor.getAttribute("href");
+			}
+		break;
+		case 39: // right arrow key
+			var anchor = document.querySelector("a[rel=next]");
+			if(anchor) {
+				document.location.href = anchor.getAttribute("href");
+			}
+		break;
+	}
+}
+document.onkeyup = document__onkeyup;
